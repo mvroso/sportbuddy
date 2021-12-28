c6009ea1b53e7cae4de730a6257b2f5e0c6b31a0
mvroso <mvroso@usp.br>
2021-12-28 13:45:12 +0100

    update base/forms/create_match

--- website/forms.py
+++ website/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField, DateTimeField, TextAreaField, SelectField
@@ -53,9 +54,9 @@ class CreateMatchForm(FlaskForm):
     title = StringField('Title',
     			validators=[DataRequired(), Length(min=2, max=50)])
     description = TextAreaField('Description')
-    # "%m/%d/%Y, %H:%M"
-    # DATA ESTA DANDO BO, PQP
-    #date = StringField('Date')
+
+    date = DateTimeField('Match Date', format='%Y-%m-%d %H:%M')#, render_kw={"placeholder": "test"})
+
     location = StringField('Location', validators=[Length(min=2, max=50)])
 
     sport_id = SelectField('Sport', choices=[(s.id, s.name) for s 
@@ -64,3 +65,8 @@ class CreateMatchForm(FlaskForm):
     #user_id = current_user.id
 
     submit = SubmitField('Create Match')
+
+    # Date validation
+    def validate_date(self, date):
+    	if date < datetime.date.today():
+    		raise ValidationError("The date cannot be in the past!")
